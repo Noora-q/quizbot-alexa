99684712c7c923f9a1b6585302493b41781320c4
Noora Al Qasimi <n.k.alqasimi@gmail.com>
2017-04-25 15:27:35 +0100

    First test passing

--- test/quizbotTest.js
+++ test/quizbotTest.js
@@ -1,19 +1,49 @@
-const context = require('aws-lambda-mock-context')
-var expect = require('chai').expect;
-var index = require('../src/index');
+var bst = require('bespoken-tools');
+var assert = require('chai').assert;
 
-const ctx = context();
+var server = null;
+var alexa = null;
 
-describe("Testing greeting message", function() {
+beforeEach(function (done) {
+    server = new bst.LambdaServer('./src/index.js', 10000, true);
+    alexa = new bst.BSTAlexa('http://localhost:10000',
+                             './speechAssets/IntentSchema.json',
+                             './speechAssets/Utterances.txt');
+    server.start(function() {
+        alexa.start(function (error) {
+            if (error !== undefined) {
+                console.error("Error: " + error);
+            } else {
+                done();
+            }
+        });
+    });
+});
 
-  var speechResponse;
-  var speechError;
+afterEach(function(done) {
+    alexa.stop(function () {
+        server.stop(function () {
+            done();
+        });
+    });
+});
 
-  before(function(done){
-    index.Handler({}, ctx);
-    ctx.Promise
-      .then(response => { speechResponse = response; console.log(speechResponse); done(); })
-      .catch(error => { speechError = error; done(); })
-  });
+it('Launches and then asks first question', function (done) {
+    // Launch the skill via sending it a LaunchRequest
+    alexa.launched(function (error, payload) {
+        // Check that the introduction is play as outputSpeech
+        console.log(payload);
+        assert.include(payload.response.outputSpeech.ssml, 'Hello World!');
 
+
+        // Emulate the user saying 'Play'
+        // alexa.spoken('Play', function (error, payload) {
+        //     // Ensure the correct directive and audioItem is returned
+        //     assert.equal(payload.response.directives[0].type, 'AudioPlayer.Play');
+        //     assert.equal(payload.response.directives[0].audioItem.stream.token, '0');
+        //     assert.equal(payload.response.directives[0].audioItem.stream.url, 'https://traffic.libsyn.com/bespoken/TIP103.mp3?dest-id=432208');
+        //     done();
+        // });
+        done();
+    });
 });
