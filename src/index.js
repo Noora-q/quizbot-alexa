873f3ac061b5aec0d4bf32f40a3ebc5a39e3ee82
Noora Al Qasimi <n.k.alqasimi@gmail.com>
2017-04-26 12:21:44 +0100

    Passed tests for newSessionHandlers

--- src/index.js
+++ src/index.js
@@ -1,19 +1,34 @@
 var Alexa = require('alexa-sdk');
+var APP_ID = undefined;
+var GAME_STATES = {
+    TRIVIA: "_TRIVIAMODE", // Asking trivia questions.
+    START: "_STARTMODE", // Entry point, start the game.
+    HELP: "_HELPMODE" // The user is asking for help.
+};
 
 exports.handler = function(event, context, callback){
   var alexa_one = Alexa.handler(event, context);
-  alexa_one.registerHandlers(handlers);
-  // alexa_one.appId = 'amzn1.ask.skill.55f71df0-c7e2-4188-ac4e-c9f19daf5636';
+  alexa_one.registerHandlers(newSessionHandlers);
+  alexa_one.appId = APP_ID;
   alexa_one.execute();
 };
 
-var handlers = {
+var newSessionHandlers = {
 
   "LaunchRequest": function () {
-    this.emit(':ask', 'Welcome to Quiz bot! Say start when you\'re ready');
+    this.emit(':ask', 'Welcome to Quiz bot! Say start when you\'re ready.');
   },
   "AMAZON.StartOverIntent": function() {
 
-    this.emit(':ask', 'Alright! Question 1. Who\'s the best coach at Makers Academy? That\'s right, it\'s Mary');
+    this.emit(':tell', 'Alright! Let\'s get started!');
+  },
+  "AMAZON.HelpIntent": function() {
+      this.emit(':ask', 'To begin the quiz, say start.');
+  },
+  "Unhandled": function () {
+      this.emit(':ask', 'Sorry, I didn\'t catch that, say start to begin.');
   }
 };
+
+
+ // 'Question 1. If 2x = 6, what is the value of x?'
