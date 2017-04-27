b710b54d821c6f36e52e1dfae047b21d2f25f201
Noora Al Qasimi <n.k.alqasimi@gmail.com>
2017-04-27 11:02:14 +0100

    Fix merge conflict

--- test/quizbotTest.js
+++ test/quizbotTest.js
@@ -33,21 +33,6 @@ it('Launches and then asks user to start', function (done) {
   alexa.launched(function (error, payload) {
     assert.include(payload.response.outputSpeech.ssml, 'Welcome to Quiz bot! Say start when you\'re ready.');
 
-<<<<<<< HEAD
-    // Emulate the user saying 'Help'
-    alexa.spoken('help', function (error, payload) {
-      assert.include(payload.response.outputSpeech.ssml, 'To begin the quiz, say start.');
-
-      // Emulate the user saying 'Start'
-      alexa.spoken('Start', function (error, payload) {
-        assert.include(payload.response.outputSpeech.ssml, 'Alright! Let\'s get started!');
-
-        // Emulate the user saying 'Help'
-        alexa.spoken('Test', function (error, payload) {
-          assert.include(payload.response.outputSpeech.ssml, 'Sorry, I didn\'t catch that, say start to begin.');
-
-          done();
-=======
    // Emulate the user saying 'Help'
     alexa.spoken('help', function (error, payload) {
       assert.include(payload.response.outputSpeech.ssml, 'To begin the quiz, say start.');
@@ -77,7 +62,6 @@ it('Launches and then asks user to start', function (done) {
             });
            });
          });
->>>>>>> 65486d2fb9307e90db575939a1facfd0e2c79a47
         });
       });
     });
